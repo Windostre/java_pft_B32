5ddb9dec1d8fc493efd14b13432f758f5021405c
Windostre <windostre@gmail.com>
2022-05-05 22:25:00 +0300

    В MyFirstProgram добавлены переменные и присвоены значения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,28 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-/* Математические выражения
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
 
-    System.out.println(1 / 2);
-    System.out.println(1.0 / 2);
-    System.out.println(1 / 2.0);
-    System.out.println(2.0 / 2);
-
-    System.out.println("2" + "2");
-    System.out.println("2" + 2);
-    System.out.println(2 + "2");
-
-    System.out.println(2 + 2 * 2);
-    System.out.println((2 + 2) * 2);
-
-    System.out.println("2 + 2 = " + 2 + 2);
-    System.out.println("2 + 2 = " + (2 + 2));
- */
+    double l = 8.0;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 
 }
