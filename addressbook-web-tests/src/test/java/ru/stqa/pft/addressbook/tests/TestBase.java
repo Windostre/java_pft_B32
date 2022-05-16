27f7aaa91185577839d0612fcdaa7ab0b342424f
Windostre <windostre@gmail.com>
2022-05-16 17:39:03 +0300

    Создан новый тест по подификации Группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,10 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
