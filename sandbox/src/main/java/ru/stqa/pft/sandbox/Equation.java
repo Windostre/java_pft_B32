d80be217e9e74511566053b162c64ac11da0d578
Windostre <windostre@gmail.com>
2022-05-18 19:19:15 +0300

    Оптимизирован алгоритм поиска кол-ва решений уравнений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -16,18 +16,7 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-
-    } else {
+    if (a != 0) {
       if (d > 0) {
         n = 2;
       } else if (d == 0) {
@@ -36,7 +25,14 @@ public class Equation {
         n = 0;
       }
 
+    } else if (b != 0) {
+      n = 1;
+    } else if (c != 0) {
+      n = 0;
+    } else {
+      n = -1;
     }
+
   }
 
   /* №1 Вложенная форма
@@ -73,6 +69,28 @@ public class Equation {
     n = 0;
   }
 }*/
+
+  /*  №4    if (a == 0) {
+    if (b == 0) {
+      if (c == 0) {
+        n = -1;
+      } else {
+        n = 0;
+      }
+    } else {
+      n = 1;
+    }
+
+  } else {
+    if (d > 0) {
+      n = 2;
+    } else if (d == 0) {
+      n = 1;
+    } else {
+      n = 0;
+    }
+
+  }*/
 
   public int rootNumber() {
     return n;
