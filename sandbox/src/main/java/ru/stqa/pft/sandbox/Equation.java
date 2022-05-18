e4492b1efee6a3f563b40151a3598183a1f0ab51
Windostre <windostre@gmail.com>
2022-05-18 18:59:21 +0300

    Доработан алгоритм проверки количества решений квадратного уравнения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -16,16 +16,63 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-      n = 2;
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
+
     } else {
-      if (d == 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
       }
+
     }
   }
+
+  /* №1 Вложенная форма
+  if (d > 0) {
+     n = 2;
+   } else {
+     if (d == 0) {
+       n = 1;
+     } else {
+       n = 0;
+     }
+   }*/
+
+  /*  № 2 Свернутая форма
+   if (d > 0) {
+    n = 2;
+  } else if (d == 0) {
+    n = 1;
+  } else {
+    n = 0;
+  }*/
+
+  /*  №3    if (a == 0) {
+    System.out.println("Это вырожденное уроавнение");
+  }
+
+    if (d > 0) {
+    n = 2;
+  }
+    if (d == 0) {
+    n = 1;
+  }
+    if (d < 0) {
+    n = 0;
+  }
+}*/
 
   public int rootNumber() {
     return n;
