9b264f8ac43f3590820d311759e0ec4f255e09dd
Windostre <windostre@gmail.com>
2022-05-23 20:11:43 +0300

    Оптимизирована навигация на странице приложения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,18 +3,42 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Group")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
+    /* Вариант 2
+    if (! isElementPresent(By.tagName("h1"))
+            || ! wd.findElement(By.tagName("h1")).getText().equals("Group")
+            || ! isElementPresent(By.name("new"))) {
+      click(By.linkText("groups"));
+    }*/
+
+
+ /*   Вариант 1
+      if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Group")
+            && isElementPresent(By.name("new"))) {
+       } else {
+        click(By.linkText("groups"));
+       }
+  }*/
 
   public void gotoHomePage() {
-    click(By.partialLinkText("home"));
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
   }
 
 }
