fb5eddef3aa695e9c70e5154579f07e73587b74d
Windostre <windostre@gmail.com>
2022-05-30 18:59:23 +0300

    Изменена форма записи массива. Добавлен цикл

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -3,10 +3,9 @@ package ru.stqa.pft.sandbox;
 public class Collections {
 
   public static void main(String[] args) {
-    String[] langs = new String[4];
-    langs[0] = "Java";
-    langs[1] = "C#";
-    langs[2] = "Python";
-    langs[3] = "PHP";
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+    for (int i = 0; i < langs.length; i++) {
+      System.out.println("Я хочу выучить " + langs[i]);
+    }
   }
 }
