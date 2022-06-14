7133f0147ca81980f9d0db8893134a50ca8c2506
Windostre <windostre@gmail.com>
2022-06-14 19:31:26 +0300

    В тесте на создание группы добавлена сортировка по id, в GroupData присвоен максимальное число по умолч для id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -42,7 +42,11 @@ public class GroupCreationTests extends TestBase {
     /*Сравнение через анонимную функциию*/
     group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group); // присваиваем новый Id для группы
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); // проверка элементов списка - списки преобразованы в множества
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+
+    Assert.assertEquals(before, after); // проверка элементов списка - списки преобразованы в множества
     app.logout(); // в лекции логаута нет
   }
 
