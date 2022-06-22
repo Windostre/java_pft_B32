c29cc8374db65da367944fbabe8b67f70bb61273
Windostre <windostre@gmail.com>
2022-06-22 18:26:25 +0300

    Лекция 5.6 Подключена бибииотека hamcrest. В тестах по созданию группы переписаны проверки с использованием методов новой библиотеки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
@@ -19,7 +24,7 @@ public class GroupCreationTests extends TestBase {
     app.group().create(group);
     /* Проверка количества групп*/
     Set<GroupData> after = app.group().all(); // количество групп после
-    Assert.assertEquals(after.size(), before.size() + 1);
+    assertThat(after.size(), equalTo(before.size() + 1));
 
     /* Проверка содержания списка групп */
     /* способ сравнения при помощи цикла
@@ -50,7 +55,8 @@ public class GroupCreationTests extends TestBase {
    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
     after.sort(byId);*/
-    Assert.assertEquals(before, after);
+    //Assert.assertEquals(before, after);
+    assertThat(after, equalTo(before));
   }
 
 }
