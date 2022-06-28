381807f5e54d281fa118c13a2a64513cd25761af
Windostre <windostre@gmail.com>
2022-06-28 13:16:09 +0300

    Лекция 5.6 Создан класс Groups.  В тестах по добавлению, модификации и удалению групп модифицирован код тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-
+import ru.stqa.pft.addressbook.model.Groups;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -18,12 +12,12 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.goTo().GroupPage();
     /* Тестовые данные */
-    Set<GroupData> before = app.group().all(); // количество групп до
+    Groups before = app.group().all(); // количество групп до
     GroupData group = new GroupData().withName("test2");
     /* Тест */
     app.group().create(group);
     /* Проверка количества групп*/
-    Set<GroupData> after = app.group().all(); // количество групп после
+    Groups after = app.group().all(); // количество групп после
     assertThat(after.size(), equalTo(before.size() + 1));
 
     /* Проверка содержания списка групп */
@@ -47,16 +41,17 @@ public class GroupCreationTests extends TestBase {
     group.setId(maxId);
     */
 
-    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()); //из коллекции after,  где есть группы с id
     // , превращаем в поток, затем этот поток превращаем в поток иденитификаторов, в качестве параметров ипм-ем анонимную функцию
     // , которая на вход получает GroupData - группу и првращает ее в целые числа
-    before.add(group);
+    //before.add(group);
    /* // Так как заменили List на all - работает с множеством, то отпадает необходимость в сортировке множетва
    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
-    after.sort(byId);*/
+    after.sort(byId);
     //Assert.assertEquals(before, after);
-    assertThat(after, equalTo(before));
+    */
+    assertThat(after, equalTo(
+            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,10 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -20,16 +27,17 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     /* Тестовые данные */
-    Set<GroupData> before = app.group().all(); // список групп до
+    Groups before = app.group().all(); // список групп до
     GroupData deletedGroup = before.iterator().next();// в  before.remove(index) не может принимать индекс, так как у нас неопределн порядок. Вводим нов объект -> сначала итератор, потом множ-во;
     /* Тест */
     app.group().delete(deletedGroup);
     /* Проверка количества групп*/
-    Set<GroupData> after = app.group().all(); // список групп после
-    Assert.assertEquals(after.size(), before.size() - 1); // проверка размеров списка
+    Groups after = app.group().all(); // список групп после
+    assertEquals(after.size(), before.size() - 1); // проверка размеров списка
     /* Проверка содержания списка групп */
-    before.remove(deletedGroup);
-    Assert.assertEquals(before, after); // проверка элементов списка цикл встроен в testng
+    //before.remove(deletedGroup);
+    assertThat(after, equalTo(before.without(deletedGroup)));
+    //Assert.assertEquals(before, after); // проверка элементов списка цикл встроен в testng
   }
 
 
